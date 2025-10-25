e2a2fff867311e2894b6930080024ac7759f29c3
EihabAhmed <eihabahmed@gmail.com>
2025-10-25 03:48:06 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const redis = require("redis");
+const { Client } = require("pg");
+const { connectionString } = require("pg/lib/defaults");
+
+// init app
+const PORT = 4000;
+const app = express();
+
+// connect to redis
+const REDIS_PORT = 6379;
+const REDIS_HOST = "redis";
+
+const redisClient = redis.createClient({
+  url: `redis://${REDIS_HOST}:${REDIS_PORT}`,
+});
+redisClient.on("error", (err) => console.log("Redis Client Error", err));
+redisClient.on("connect", () => console.log("connected to redis..."));
+redisClient.connect();
+
+// connect mongo db
+const DB_USER = "root";
+const DB_PASSWORD = "example";
+const DB_PORT = 27017;
+const DB_HOST = "mongo";
+
+const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
+mongoose
+  .connect(URI)
+  .then(() => console.log("connected to db ..."))
+  .catch((err) => console.log("failed to connect to db: ", err));
+
+// connect postgres db
+// connect db
+const POSTGRES_DB_USER = "root";
+const POSTGRES_DB_PASSWORD = "example";
+const POSTGRES_DB_PORT = 5432;
+const POSTGRES_DB_HOST = "postgres";
+
+const POSTGRES_URI = `postgres://${POSTGRES_DB_USER}:${POSTGRES_DB_PASSWORD}@${POSTGRES_DB_HOST}:${POSTGRES_DB_PORT}`;
+const client = new Client({
+  connectionString: POSTGRES_URI,
+});
+client
+  .connect()
+  .then(() => console.log("connected to postgres db ..."))
+  .catch((err) => console.log("failed to connect to postgres db: ", err));
+
+app.get("/", (req, res) => {
+  redisClient.set("products", "products...");
+  res.send("<h1> Hello Tresmerge!</h1>");
+});
+
+app.get("/data", async (req, res) => {
+  const products = await redisClient.get("products");
+  res.send(`<h1>Hello Tresmergs!</h1> <h2>${products}</h2>`);
+});
+
+app.listen(PORT, () => console.log(`app is up and running on port: ${PORT}`));

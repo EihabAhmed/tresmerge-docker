7fd7a231d6fdeebfeb1d0b7898339a4d39fff73b
EihabAhmed <eihabahmed@gmail.com>
2025-10-25 04:56:19 +0300

    updated return message

--- src/index.js
+++ src/index.js
@@ -49,7 +49,7 @@ mongoose
 
 app.get("/", (req, res) => {
   redisClient.set("products", "products...");
-  res.send("<h1> Hello Tresmerge!</h1>");
+  res.send("<h1> Hello Tresmerge! from Oracle</h1>");
 });
 
 app.get("/data", async (req, res) => {
